044e16ef30b0988c2a11bb4a031c9348690f560c
rokysf <135695631+rokysf@users.noreply.github.com>
2023-10-17 20:41:05 +0200

    Add files via upload

---
+++ S04T01N03/Projecte Gradle.js
@@ -0,0 +1,21 @@
+{
+	"id": "e71fda49-1f9c-429a-b6fc-d1004442d383",
+	"name": "Projecte Gradle",
+	"values": [
+		{
+			"key": "Server",
+			"value": "http://localhost",
+			"type": "default",
+			"enabled": true
+		},
+		{
+			"key": "Port",
+			"value": ":9001",
+			"type": "default",
+			"enabled": true
+		}
+	],
+	"_postman_variable_scope": "environment",
+	"_postman_exported_at": "2023-10-17T18:36:25.794Z",
+	"_postman_exported_using": "Postman/10.19.5"
+}
---
+++ S04T01N03/Projecte Maven.js
@@ -0,0 +1,21 @@
+{
+	"id": "20859162-5929-4a7b-a810-4c3b05d5ccf1",
+	"name": "Projecte Maven",
+	"values": [
+		{
+			"key": "Server",
+			"value": "http://localhost",
+			"type": "default",
+			"enabled": true
+		},
+		{
+			"key": "Port",
+			"value": ":9000",
+			"type": "default",
+			"enabled": true
+		}
+	],
+	"_postman_variable_scope": "environment",
+	"_postman_exported_at": "2023-10-17T18:37:09.050Z",
+	"_postman_exported_using": "Postman/10.19.5"
+}
